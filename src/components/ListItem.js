843a8fb2b68572a840a15df4a89012d4e4f58172
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-07-11 20:12:57 +0200

    Pass weatherData props to UpcomingWeather

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import moment from 'moment';
+
+import { weatherType } from '../utilities/weatherType';
 
 const ListItem = (props) => {
     const { dt_text, min, max, condition } = props;
-    const { date, item, temp } = styles;
+    const { date, dateTextWrapper, item, temp } = styles;
     return (
         <View style={item}>
-            <Feather name={'sun'} size={50} color={'white'} />
-            <Text style={date}>{dt_text}</Text>
-            <Text style={temp}>{min}</Text>
-            <Text style={temp}>{max}</Text>
+            <Feather
+                name={weatherType[condition].icon}
+                size={50}
+                color={'white'}
+            />
+            <View style={dateTextWrapper}>
+                <Text style={date}>{moment(dt_text).format('dddd')}</Text>
+                <Text style={date}>{moment(dt_text).format('h:mm:ss a')}</Text>
+            </View>
+            <Text style={temp}>{`${Math.round(min)}°/${Math.round(
+                max,
+            )}°`}</Text>
         </View>
     );
 };
@@ -19,6 +30,9 @@ const styles = StyleSheet.create({
     date: {
         color: 'white',
         fontSize: 15,
+    },
+    dateTextWrapper: {
+        flexDirection: 'column',
     },
     item: {
         alignItems: 'center',
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -5,54 +5,14 @@ import {
     SafeAreaView,
     StyleSheet,
     StatusBar,
-    Text,
 } from 'react-native';
 import ListItem from '../components/ListItem';
 
-const DATA = [
-    {
-        dt_text: '2023-02-18 12:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55,
-        },
-        weather: [
-            {
-                main: 'Clear',
-            },
-        ],
-    },
-    {
-        dt_text: '2023-02-18 15:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55,
-        },
-        weather: [
-            {
-                main: 'Clouds',
-            },
-        ],
-    },
-    {
-        dt_text: '2023-02-18 18:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55,
-        },
-        weather: [
-            {
-                main: 'Rain',
-            },
-        ],
-    },
-];
-
-const UpcomingWeather = () => {
+const UpcomingWeather = ({ weatherData }) => {
     const renderItem = ({ item }) => (
         <ListItem
             condition={item.weather[0].main}
-            dt_text={item.dt_text}
+            dt_text={item.dt_txt}
             min={item.main.temp_min}
             max={item.main.temp_max}
         />
@@ -65,11 +25,10 @@ const UpcomingWeather = () => {
                 source={require('../../assets/upcoming-background.jpg')}
                 style={image}
             >
-                <Text>Upcoming Weather</Text>
                 <FlatList
-                    data={DATA}
+                    data={weatherData}
                     renderItem={renderItem}
-                    keyExtractor={(item) => item.dt_text}
+                    keyExtractor={(item) => item.dt_txt}
                 />
             </ImageBackground>
         </SafeAreaView>
